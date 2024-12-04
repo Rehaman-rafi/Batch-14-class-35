8cd12d273989d750f99e9dbdf15ab80d976e280a
Rehaman-rafi <Rehamanrafi19@gmail.com>
2024-12-04 11:39:22 +0600

    PHP

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+// function test(){
+//     echo "Hello";
+// }
+// test();
+
+// function sum(){
+//     $x = 10;
+//     $y = 30;
+
+//     // echo $result = $x + $y;
+//     $result = $x + $y;
+//     return $result;
+// }
+// // sum();
+// echo sum()."<br>";
+// $result = sum() + 40;
+// echo $result;
+
+function sum($x,$y){
+      $result = $x + $y;
+      return $result;
+}
+// echo sum(100,200)."<br>";
+// $result2 = sum(20,30) + 100;
+// echo $result2."<br>";
+// echo sum(10,20) + 30;
+
+function multiply($number){
+    $result = sum(20,20);
+   $multiply = $result*$number;
+   return $multiply;
+}
+echo multiply(10);
+
+
+ 
+?>
